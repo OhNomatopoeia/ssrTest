4d7928f79fcf34742ee80969430eada97cc2a253
OhNomatopoeia <rljpires90@gmail.com>
2021-05-24 00:58:28 +0100

    request files done

---
+++ src/components/search-bar/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          items: []
+        };
+      }
+
+    render(){
+        return(
+         <div>hello</div>
+        )
+    }
+}
+
+
+export default SearchBar;
---
+++ src/components/search-entry/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class SearchEntry extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          error: null,
+          isLoaded: false,
+          items: []
+        };
+      }
+
+    render(){
+        return(
+         <div>hello</div>
+        )
+    }
+}
+
+
+export default SearchEntry;
